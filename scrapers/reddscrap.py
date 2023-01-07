38e814591653f1f1f26cf3f0a546f489041552b8
Shubham Dhapola <dhapola.shubham321@gmail.com>
2023-01-07 18:38:00 +0530

    Update 'scrapers/' - Update 'reddscrap.py'

--- scrapers/reddscrap.py
+++ scrapers/reddscrap.py
@@ -1,25 +1,29 @@
+from urllib.parse import quote
 import requests
 from bs4 import BeautifulSoup as bsoup
 
-url = "https://old.reddit.com/r/datascience"
+url = r"https://old.reddit.com/r/datascience"
+base_url = r"https://old.reddit.com"
 # Headers to mimic a browser visit
 headers = {
     'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 \
             Safari/537.36'
 }
-
+# sanitize urls
+url = quote(url, safe=":/?&")
+base_url = quote(base_url, safe=":/?&")
 # Returns a requests.models.Response object 
 page = requests.get(url, headers=headers)
+soup = bsoup(page.text, 'lxml')
+# outer containers
+top_matter = soup.find_all("div", attrs={"class": "top-matter"})
+# all further approachable urls
+all_urls = [
+    quote(base_url + outer_container.find("a", attrs={"class": "title may-blank"})["href"], safe=":/?&") \
+        for outer_container in top_matter if outer_container.find("a", attrs={"class": "title may-blank"})]
+# We get some extra urls using the strategy used above, collect all comments
+comments = [url for url in all_urls if "https://old.reddit.com/r/datascience" in url]
 
-soup = bsoup(page.text, 'html.parser')
-
-domains = soup.find_all("span", class_="domain")
-
-soup.find_all("span", {"class": "domain", "height": "100px"})
-
-#for domain in domains:
-#    if domain != "(self.datascience)":
-#        continue
-#    print(domain.text)
-
+# Make request for each url
+soups = [bsoup(requests.get(url, headers=headers).text, 'lxml') for url in comments]
